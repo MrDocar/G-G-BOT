d519e80ee0bee2836884d746834e078a7c5e92f9
MrDocar <alex.didikong@gmail.com>
2018-08-28 19:30:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 /* Tableau */
 var insulte = ['connard', 'encule', 'salope', 'fdp', "fils de pute", 'va te faire enculer', 'bande d’abrutis','encule de ta race', 'bounioule', 'batard', 'garage a bite', 'branleur', 'mongol','nique ta mere', 'ntm','niquer sa mere', 'negro','ta mere la pute', 'ta race', 'tapette', 'tantouze','tafiole','travail d’arabe','trou du cul','petite bite','petite merde','zguègue','pute','putain de ta race','putain','poufiasse','pouffiasse','pouf','pedale','pd','tete de gland'];
-
+var valide = ['OK','TKT','XD','OKAY','LOL','MDR'];
 /* Objet bot */
 var bot = new Discord.Client();
 
@@ -16,27 +16,40 @@ bot.on('ready', () => {
 });
 
 /* Connexion du bot */
-bot.login(process.env.TOKEN);
+bot.login('process.env.TOKEN');
+
 /* Réception d'un message */
 bot.on('message',message => {
     /* Je vérifie que c'est pas un message du bot */
-    if (message.author.id != 478284566130327554) {
-        /* Déclaration des variable */
-        strings = no_accent(message.content); // Récupération du message
-        i=0;
-        calcule = 0;
-        character='';
+    if (message.author != '<@478284566130327554>' || message.author != '<@446716898365669376>' || message.author != '<@155149108183695360>' ) {
+           
 
-        /* Boucle qui analize chaque lettre */
-        for (let i = 0; i < strings.length; i++) {
-            character = strings.charAt(i);
-            if (isEmoji(message.content) || character != '!' || character != '?' || character != '.') {
-                null;
-            } else {
-                /* Détection si c'est un caratere numerique */
-                if (!isNaN(character * 1)){
-                    calcule = calcule+1; // on ajoute 1 a la variable "calcule"
+        if (message.attachments.size == 0 && message.embeds.length == 0)  {
+            
+            /* Déclaration des variable */
+            strings = no_accent(message.content); // Récupération du message
+            i=0;
+            calcule = 0;
+            character='';
+
+            for (let x = 0; x < insulte.length; x++) {
+                stringslower = strings.toLowerCase(); //on mes tout en minuscule
+                var Resultat = stringslower.indexOf(insulte[x]);
+                if (Resultat != -1) { //Si le mot est dans le tableau d'insulte on sanctionne
+                    quarantaine("Insulte");
+                    x = insulte.length;
+                }
+            }
+
+            for (let y = 0; y < strings.length; y++) {
+                character = strings.charAt(y);
+                if (isEmoji(message.content) || character == '!' || character == '?' || character == '.') {
+                    null;
                 } else {
+                    /* Détection si c'est un caratere numerique */
+                    if (!isNaN(character * 1)){
+                        calcule = calcule+1; // on ajoute 1 a la variable "calcule"
+                    }
                     /* Détection si c'est un caratere majuscule */
                     if (character == character.toUpperCase()) {
                         calcule = calcule+1; // on ajoute 1 a la variable "calcule"
@@ -44,21 +57,17 @@ bot.on('message',message => {
                 }
             }
 
-        }
+            /* Boucle qui analize chaque lettre */
+            for (let i = 0; i < strings.length; i++) {
+                Resultatvalide = strings.indexOf(valide[i]);
+                if (Resultatvalide != -1) {
+                    calcule = calcule - valide[i].length;
+                }
+            }
 
-        /* On regarde si le nombre de majuscule et de caratere numérique est égale a nombre de caratere dans le message */
-        if (calcule == strings.length) {
-            quarantaine("Caps Lock");
-        }
-
-        /* Boucle qui analize chaque mot */
-        for (let x = 0; x < insulte.length; x++) {
-            strings = strings.toLowerCase(); //on mes tout en minuscule
-
-            var Resultat = strings.indexOf(insulte[x]);
-            if (Resultat != -1) { //Si le mot est dans le tableau d'insulte on sanctionne
-                quarantaine("Insulte");
-                x = insulte.length;
+            /* On regarde si le nombre de majuscule et de caratere numérique est égale a nombre de caratere dans le message */
+            if (calcule == strings.length) {
+                quarantaine("Caps Lock");
             }
         }
     }
